1c8f7c7ef629398ba297401ae97865944b830794
Vlad Vasiliev <vlad@example.com>
2024-09-27 15:12:29 +0300

    Commit 02

--- cats/serializers.py
+++ cats/serializers.py
@@ -11,8 +11,8 @@ class AchievementSerializer(serializers.ModelSerializer):
 
 
 class CatSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField(read_only=True)
-    achievements = AchievementSerializer(read_only=True, many=True)
+    # owner = serializers.StringRelatedField(read_only=True)
+    achievements = AchievementSerializer(many=True) #, read_only=True)
 
     class Meta:
         model = Cat
